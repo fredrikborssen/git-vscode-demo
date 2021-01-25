4d72fbb0184ace64cb3cdb455911bd4dc46c0b8a
Fredrik <fredrikborssen@gmail.com>
2021-01-25 22:36:05 +0100

    Added Searchbar
    la till en searchbar som kan användas senare samt bara som övning för mig själv

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React ,{useState} from 'react'
+import JSONDATA from '../Data/MOCK_DATA.json'
 const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('')
     return (
         <div>
             <h1>Welcome Home</h1>
+            <div className="App">
+                <input type="text" placeholder="Search..." onChange={event => {setSearchTerm(event.target.value)}}/>
+                {JSONDATA.filter((val => {
+                    if(searchTerm == "")
+                    {
+                        return val
+                    } else if(val.first_name.toLowerCase().includes(searchTerm.toLocaleLowerCase())){
+                        return val.first_name
+                    }
+                })).map((val, key)=> {
+                    return (<div className="user" key={key}> {val.first_name} </div>);
+                })
+                }
+            </div>
         </div>
     )
 }
